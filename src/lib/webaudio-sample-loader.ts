e6bda74d2fab1222e937b8e3f748ed91ba1dbac1
Peter van der Noord <peterdunord@gmail.com>
2018-03-18 13:15:42 +0100

    Remove loadAudioBuffer method (add audiobuffer-loader dependency)

--- src/lib/webaudio-sample-loader.ts
+++ src/lib/webaudio-sample-loader.ts
@@ -1,3 +1,5 @@
+import { loadAudioBuffer } from 'audiobuffer-loader';
+
 export interface ISample {
   name: string; // filename without extensions
   extension?: string; // forces an extension for this sample
@@ -17,11 +19,6 @@ export function createSample(name: string, extension?: string, path?: string): I
     extension,
     path,
   };
-}
-
-interface ILoadAudioBufferResult {
-  audioBuffer: AudioBuffer;
-  fileSize: number;
 }
 
 /**
@@ -71,64 +68,4 @@ export function loadSamples(
     });
   });
   return Promise.all(loadPromises).then(() => samples);
-}
-
-/**
- * Loads and decodes an audio-file, resulting in an AudioBuffer and the fileSize of the loaded file.
- * @param {AudioContext} context
- * @param {string} url
- * @param {(value: number) => void} onProgress
- * @returns {Promise<ILoadAudioBufferResult>}
- */
-function loadAudioBuffer(
-  context: AudioContext,
-  url: string,
-  onProgress: (value: number) => void,
-): Promise<ILoadAudioBufferResult> {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-
-    // listen to onprogress events if onProgress was supplied
-    if (onProgress) {
-      request.onprogress = event => {
-        onProgress(event.loaded / event.total);
-      };
-    }
-
-    request.onload = () => {
-      if (request.status === 200) {
-        const fileSize = request.response.byteLength;
-        const decodePromise = context.decodeAudioData(
-          request.response,
-          audioBuffer => {
-            resolve({
-              audioBuffer,
-              fileSize,
-            });
-          },
-          error => {
-            reject(error);
-          },
-        );
-
-        // not all implementations use this promise (was later added to the API). if they do
-        // we're catching (and ignoring) errors that may occur through here. these errors are
-        // handled by the onError method in the decodeAudioData call (3rd param)
-        if (decodePromise) {
-          decodePromise.catch(() => {});
-        }
-      } else {
-        reject(`Error loading '${url}' (${request.status})`);
-      }
-    };
-
-    request.onerror = error => {
-      reject(error);
-    };
-
-    request.send();
-  });
 }
