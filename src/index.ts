a2cde080b75a3eda65b57ffeb84cd6eabc900fe4
Peter van der Noord <peterdunord@gmail.com>
2018-03-22 20:21:31 +0100

    Expose ISample and ICreateSample interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as SampleManager } from './lib/SampleManager';
 
 export { createSample, createSamplesFromNames, loadSamples } from './lib/utils';
+
+export { ISample, ICreateSample } from './lib/interface';
